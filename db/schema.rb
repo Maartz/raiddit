95679f8b69fc8d914333f5876ccf08e59a718ebb
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-13 21:11:46 +0100

    Fixed missing video submissions migration and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_172316) do
+ActiveRecord::Schema.define(version: 2020_03_13_201006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_172316) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "submission_image"
     t.bigint "user_id"
+    t.string "submission_video"
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
