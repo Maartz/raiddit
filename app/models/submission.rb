6e2096a65aed595e43d8ea22715adbf0b4b74128
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-20 07:15:12 +0100

    Create comments model | Create model migration | Update submission model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,6 +4,10 @@ class Submission < ApplicationRecord
   belongs_to :user
   belongs_to :community
 
+  # Destroy the comments if the submission is deleted
+  # Links the comment and the submission
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, length: { maximum: 8000 }
   validates :url, url: { allow_blank: true }
