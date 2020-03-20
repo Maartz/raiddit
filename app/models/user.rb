836af4e4b2c2358fd3d2cfac27ec31bcf6e86b6b
Maartz <maartz@MacBook-Pro-de-Maartz.local>
2020-03-20 07:31:32 +0100

    Update comment to get user_id column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   has_many :communities
   has_many :submissions
+  has_many :comments
 
   validates_uniqueness_of :username
   validates_presence_of :username
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_053010) do
+ActiveRecord::Schema.define(version: 2020_03_20_062716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2020_03_20_053010) do
     t.bigint "submission_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["submission_id"], name: "index_comments_on_submission_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "communities", force: :cascade do |t|
@@ -64,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_053010) do
   end
 
   add_foreign_key "comments", "submissions"
+  add_foreign_key "comments", "users"
   add_foreign_key "communities", "users"
   add_foreign_key "submissions", "communities"
   add_foreign_key "submissions", "users"
